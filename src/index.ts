80e6e622d91eaa7f59bcc6e82006211bf47ad740
Mikkel RINGAUD <mikkel@milescode.dev>
2022-05-30 00:42:31 +0200

    fix: eslint

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import startServer from "./app/startServer.js";
 import parseArgs from "minimist";
 
 const root_folder_path = process.argv[2];
-const args = parseArgs(process.argv.slice(3))
+const args = parseArgs(process.argv.slice(3));
 
 // Check if device was specified.
 if (!root_folder_path) throw Error(
